fc9a8b72918bc46bd233307db7b7c027312f695a
Shahd <shahad9mubarak@gmail.com>
2026-04-08 14:46:22 +0400

    Task 16: only14

---
+++ src/TASK16.java
@@ -0,0 +1,18 @@
+public class TASK16 {
+    static void main(String[] args) {
+        int [] arr = {1,5,3,6};
+        System.out.println(only14(arr));
+
+    }
+    public static boolean  only14(int[] arr){
+        for (int i = 0 ; i< arr.length ; i++){
+            if (arr[i] != 1){
+                if (arr [i] != 4){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
