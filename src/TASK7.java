3126dc2381d5e2f3d5b7ddd8f5ec7d1af3b46d21
Shahd <shahad9mubarak@gmail.com>
2026-04-08 11:21:40 +0400

    Task 7: modThree

--- src/TASK7.java
+++ src/TASK7.java
@@ -1,2 +1,17 @@
 public class TASK7 {
+    static void main(String[] args) {
+        System.out.println(modThree(new int[]{2,4,6,1,3}));
+
+    }
+    public static boolean modThree(int[] arr){
+        for (int i=0 ; i<arr.length ; i++){
+            if (arr[i] % 2 ==0  && arr[i+1] % 2 == 0 && arr[i+2] %2==0 ){
+                return true;
+            }
+            if (arr[i] % 2 !=0 && arr[i+1] % 2 != 0 && arr[i+2] %2!=0 ){
+                return true;
+            }
+        }
+        return false;
+    }
 }
