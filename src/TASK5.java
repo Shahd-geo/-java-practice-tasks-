b9734b6d922222fe498ff4f4c4ee54d0adfc67d0
Shahd <shahad9mubarak@gmail.com>
2026-04-08 10:53:29 +0400

    Task 5: no14

--- src/TASK5.java
+++ src/TASK5.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
 
 public class TASK5 {
+    static void main(String[] args) {
+        System.out.println(no14(new int[]{1, 7, 8, 9}));
+        System.out.println(no14(new int[]{1, 4, 8, 9}));
+
+    }
+
+    public static boolean no14(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                for (int s = 0; s < arr.length; s++) {
+                    if (arr[s] == 4) {
+                        return false;
+                    }
+                }
+
+            }
+
+        } return true;
+    }
 }
