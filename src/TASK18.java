3fc7a3e852c64942fd93486462197cce8460e0eb
Shahd <shahad9mubarak@gmail.com>
2026-04-08 14:57:39 +0400

    Task 18: has77

---
+++ src/TASK18.java
@@ -0,0 +1,17 @@
+public class TASK18 {
+    static void main(String[] args) {
+        int[] arr = {1,2,6,7,8,7};
+        System.out.println(has77(arr));
+
+    }
+    public static boolean has77(int[] arr){
+        for (int i = 0 ; i<arr.length ; i++){
+            if (arr[i]==7 && arr[i+1]==7){
+                return true;
+            }if (arr[i]==7 && arr[i+2]==7){
+                return true;
+            }
+        }
+        return false;
+    }
+}
