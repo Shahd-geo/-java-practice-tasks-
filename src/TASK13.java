53e3360c3af2db9cd181ce6baf69b2d8bda48e22
Shahd <shahad9mubarak@gmail.com>
2026-04-08 14:18:57 +0400

    Task 13: bigDiff

--- src/TASK13.java
+++ src/TASK13.java
@@ -1,2 +1,20 @@
 public class TASK13 {
+    static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        System.out.println( bigDiff(arr));
+
+    }
+    public static int  bigDiff(int [] arr){
+        int max = arr[0];
+        int min = arr[0];
+        int result = 0;
+        for(int n :arr ) {
+            if (n > max) {
+                max = n;
+            } else if (n < min) {
+                min=n;
+
+            }result= max-min;
+        }return result;
+    }
 }
